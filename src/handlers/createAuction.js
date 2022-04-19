d3541922cbf6f528a0b741533068f57fdd93e5da
Uladzislau Kavalski <vladislav.kovalskij@gmail.com>
2022-04-19 07:31:12 +0300

    save auction to DB

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,14 +1,27 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+
 import { AUCTION_STATUS } from '../constants';
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event) {
   const { name } = JSON.parse(event.body);
   const now = new Date();
 
   const auction = {
+    id: uuid(),
     name,
     status: AUCTION_STATUS.open,
     createdAt: now.toISOString(),
   };
+
+  await dynamoDB
+    .put({
+      TableName: 'AuctionTable',
+      Item: auction,
+    })
+    .promise();
 
   return {
     statusCode: 201,
