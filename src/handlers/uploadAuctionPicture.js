3cba5ad1ad1546655d7bcf158059e866297a0f9a
Uladzislau Kavalski <vladislav.kovalskij@gmail.com>
2022-05-19 00:12:45 +0300

    upload picture to s3

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -1,8 +1,31 @@
-async function uploadAuctionPicture() {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({}),
-  };
+import createError from 'http-errors';
+import AuctionRepository from '../repositories/auctionRepository';
+import { uploadPictureToS3 } from '../util/auploadPictureToS3';
+
+const auctionRepository = new AuctionRepository();
+
+async function uploadAuctionPicture(event) {
+  const {
+    body,
+    pathParameters: { id },
+  } = event;
+
+  const auction = await auctionRepository.findById(id);
+
+  const base64 = body.replace(/^data:image\/\w+;base64,/, '');
+  const buffer = Buffer.from(base64, 'base64');
+  const imageName = `${auction.id}.jpg`;
+
+  try {
+    const uploadResult = await uploadPictureToS3(imageName, buffer);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(uploadResult),
+    };
+  } catch (error) {
+    throw new createError.InternalServerError(error);
+  }
 }
 
 export const handler = uploadAuctionPicture;
